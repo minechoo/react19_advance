83522f87195abc93e34335fc670f8043f58e675a
minechoo <choominhae@gamil.com>
2025-05-11 12:57:37 +0900

    전역 컨텍스트 값으로 배경, 글자 색 분기처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import { useContext } from "react";
 import { Route, Routes} from "react-router-dom"
 import MovieDetail from "./Pages/MovieDetail";
 import MovieList from "./Pages/MovieList";
+import { ThemeContext } from "./ThemeProvider";
 
 
 function App() {
+  const {GlobalState} = useContext(ThemeContext)
 
+  const styleLight = {
+    backgroundColor: "white",
+    color: "black",
+  };
+
+  const styleDark = {
+    backgroundColor: "black",
+    color: "white",
+  };
   return (
     <>
-      <Routes>
-      <Route path="/" element={<MovieList />} />
-      <Route path="/detail/:id" element={<MovieDetail />} />
-    </Routes>
+      <main style={GlobalState.isLight ? styleLight : styleDark}>
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/detail/:id" element={<MovieDetail />} />
+        </Routes>
+      </main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { ThemeProvider } from './Pages/ThemeProvider.jsx'
+import { ThemeProvider } from './ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
