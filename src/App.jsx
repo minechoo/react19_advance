09f16990f79228f3178507615b014fb2aac9da1e
minechoo <choominhae@gmail.com>
2025-05-17 14:49:30 +0900

    useDeferredValue의 기념 및 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,32 @@
-import { useState, useTransition } from 'react';
+import { useDeferredValue } from 'react';
+import { useState } from 'react';
 
 export default function App() {
 	const [InputValue, setInputValue] = useState('');
-	const [MovieData, setMovieData] = useState([]);
-
-	const [isPending, startTransition] = useTransition();
+	//useDeferredValue를 통해서 특정 상태값을 지연시켜서 새로운 상태값 생성
+	const deferredValue = useDeferredValue(InputValue);
+	//새로운 상태값을 활용해서 1만번 반복도는 무거운 로직을 일부러 실행
+	//이때 deferredValue로 상태값 반영자체가 지연되기 때문에 InputValue상태값이 바뀔때마다 해당 repeat함수가 바로바로 실행되는 것이아닌
+	//일정한 지연텀을 두면서 호출되므로 resultValue에 대한 재랜더링 처리는 줄어듬 (리액트의 렌더링 소비비용 감소)
+	const resultValue = deferredValue.repeat(10000);
 
 	const handleChange = (e) => {
 		setInputValue(e.target.value);
-
-		// 우선순위를 낮춰야 되는 상태 변경로직 자체를 startTransition에 콜백함수 형태로 전달
-		// 해당 콜백 안쪽에 있는 상태 업데이트 로직은 우선순위가 낮게 설정됨
-		// 화면에 긴급하게 반영해야되는 상태 변경에 영향을 미치지 않음
-		startTransition(() => {
-			const newData = [];
-			for (let i = 0; i < 3000000; i++) {
-				newData.push(i);
-			}
-
-			setMovieData(newData);
-		});
 	};
 
 	return (
 		<>
 			<h1>useTransition</h1>
-
 			<input type='text' value={InputValue} onChange={handleChange} />
-			<p>{isPending ? '연산 처리중...' : '연산 완료'}</p>
+			<p>지연처리된 상태값 : {resultValue}</p>
 		</>
 	);
 }
 
 /*
-  useTransition
-  - 리액트 컴포넌트에 state값의 변경처리의 우선순위를 차등해서 긴급한 상태변경과, 덜 긴급한 상태변경사항을 관리하는 훅
-  - useTransition으로 상태 업데이트의 순서의 우선 순위를 차등 적용해야 되는 사례
-  - 폼요소에 입력값을 바탕으로 실시간으로 서버 데이터 호출 및 렌더링 해야될때
-
-  - 작업순서1-인풋에 검색어 입력 -> 인풋요소에 입력하고 있는 검색내용이 출력됨
-  - 작업순서2-인풋에 반영되고 있는 상태값에 따라 서버 데이터 fetching이나 기타 무거운 로직 수행
-  - 작업순서3로직수행이 완료되면 완료된 데이터를 다시 새로운 상태에 담아서 화면에 출력
-
-  위와 같은 순서의 작업을 할때 상태값이 2개 필요 (input출력용 상태값, 수행완료된 서버데이터를 화면에 출력용 상태값)
-  -input용 상태값은 로직처리가 무겁지 않고 화면에 바로바로 반영해야 되는 데이터 (Urgent Update) 긴급 변경필요 데이터
-  -서버 데이터 출력용 상태값은 로직처리가 무겁과 화면에 바로바로 출력할 필요가 없는 데이터 (Not Urgent Update) 비긴급 변경필요 데이터
-
-  [강제로우선순위를 낮추 상태값의 변경완료유무, 특정 상태값의 우선순위를 낮추는 함수] = useTransition
-  const [isPending, startTransition] = = useTransition()
+  useDeferredValue
+  - useTransition처럼 실제 상태값의 우선순위를 연산을 지연시키는 것이 아닌 특정 상태값의 사용을 지연시킴
+  - 물리적인 상태값 연산의 지연이 아닌 그냥 상태값 활용을 지연시키는 이유 -> 불필요한 렌더링 횟수 줄이기 위함
+  - useTransition이 복수개의 상태값중에서 특정 상태값의 연산자체를 지연시키는 만면
+  - useDefferedValue는 그냥 하나의 값의 사용자체를 지연시킴
 */
